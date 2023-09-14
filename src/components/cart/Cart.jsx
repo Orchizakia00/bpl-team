956c44c355959e766fd5451a57cb8b4bf57e478e
Orchizakia00 <zakiaislam290@gmail.com>
2023-09-14 16:41:36 +0600

    cart created

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
 
-const Cart = () => {
+const Cart = ({ selectedPlayers }) => {
+    console.log(selectedPlayers);
+
     return (
         <div>
-            
+            <p className="text-3xl font-bold text-center mb-4">Player Cart</p>
+            <h3 className="text-center mb-4">Total Players: {selectedPlayers.length}</h3>
+            {
+                selectedPlayers.map((player)=>(
+                    <li>{player.name}</li>
+                ))
+            }
         </div>
     );
 };
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/jsx-key */
 import { useEffect } from "react";
 import { useState } from "react";
+import Cart from "../cart/Cart";
 
 const Home = () => {
 
     const [players, setPlayers] = useState([]);
+    const [selectedPlayers, setSelectedPlayers] = useState([]);
 
     useEffect(() => {
         fetch('./players.json')
@@ -12,13 +15,18 @@ const Home = () => {
     }, [])
     // console.log(players);
 
-    return (
-        <div className=" grid grid-cols-2 gap-4">
+    const handleSelectBtn = (player) => {
+        setSelectedPlayers([...selectedPlayers, player]);
+    }
+    console.log(selectedPlayers);
 
+    return (
+        <div className="flex gap-10">
+            <div className="grid grid-cols-2 gap-4 w-3/4">
             {
                 players.map(player => (
-                    <>
-                        <div className="border-2 bg-green-200 rounded-2xl p-4 w-96">
+                    
+                        <div key={player.id} className="border-2 bg-green-200 rounded-2xl p-4 w-96">
                             <div className="flex justify-center">
                                 <img className="rounded-full w-40 h-40" src={player.img} alt="" />
                             </div>
@@ -26,15 +34,22 @@ const Home = () => {
                             <p className="text-center mb-2">{player.description}</p>
                             <div className="flex gap-3 justify-center mb-2">
                                 <p><span className="font-bold">Salary: </span>${player.salary}</p>
-                                <p><span className="font-bold">Role:</span>{player.role}</p>
+                                <p><span className="font-bold">Role: </span>{player.role}</p>
                             </div>
                             <div className="flex justify-center">
-                                <button className="bg-orange-400 p-3 rounded-xl text-center">Select</button>
+                                <button onClick={() => handleSelectBtn(player)} className="bg-orange-400 p-3 rounded-xl text-center">Select</button>
                             </div>
                         </div>
-                    </>
                 ))
             }
+            </div>
+
+            <div className="px-5 rounded-xl bg-red-200 w-1/4">
+                <Cart
+                selectedPlayers = {selectedPlayers}
+                ></Cart>
+            </div>
+
         </div>
     );
 };
