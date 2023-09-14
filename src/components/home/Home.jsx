df243bc447b07bf4eb004b8ec470a0b5bfddebe9
Orchizakia00 <zakiaislam290@gmail.com>
2023-09-14 13:32:05 +0600

    dynamically added all cards

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,25 +5,36 @@ const Home = () => {
 
     const [players, setPlayers] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('./players.json')
-        .then(res => res.json())
-    },[])
+            .then(res => res.json())
+            .then(data => setPlayers(data))
+    }, [])
+    // console.log(players);
 
     return (
-        <div className="border-2 w-64 bg-green-200 rounded-2xl p-4">
-            <div className="flex justify-center">
-                <img className="rounded-full w-40 h-40" src="https://m.cricbuzz.com/a/img/v1/192x192/i1/c170916/mushfiqur-rahim.jpg" alt="" />
-            </div>
-            <p className="text-center font-bold">Mushfiq</p>
-            <p className="text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, nam.</p>
-            <div className="flex gap-3 justify-center">
-                <p>Salary: $1.2M</p>
-                <p>Wicket Keeper</p>
-            </div>
-            <div className="flex justify-center">
-            <button className="bg-orange-400 p-3 rounded-xl text-center">Select</button>
-            </div>
+        <div className=" grid grid-cols-2 gap-4">
+
+            {
+                players.map(player => (
+                    <>
+                        <div className="border-2 bg-green-200 rounded-2xl p-4 w-96">
+                            <div className="flex justify-center">
+                                <img className="rounded-full w-40 h-40" src={player.img} alt="" />
+                            </div>
+                            <p className="text-center font-bold mb-2">{player.name}</p>
+                            <p className="text-center mb-2">{player.description}</p>
+                            <div className="flex gap-3 justify-center mb-2">
+                                <p><span className="font-bold">Salary: </span>${player.salary}</p>
+                                <p><span className="font-bold">Role:</span>{player.role}</p>
+                            </div>
+                            <div className="flex justify-center">
+                                <button className="bg-orange-400 p-3 rounded-xl text-center">Select</button>
+                            </div>
+                        </div>
+                    </>
+                ))
+            }
         </div>
     );
 };
