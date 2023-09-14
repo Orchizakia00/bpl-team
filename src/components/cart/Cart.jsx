205829c4c7faf576c957f84e8843454391e5145b
Orchizakia00 <zakiaislam290@gmail.com>
2023-09-14 17:44:53 +0600

    total cost and remaining balance section done

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
 
-const Cart = ({ selectedPlayers }) => {
+const Cart = ({ selectedPlayers, remaining, totalCost }) => {
     console.log(selectedPlayers);
 
     return (
         <div>
             <p className="text-3xl font-bold text-center mb-4">Player Cart</p>
             <h3 className="text-center mb-4">Total Players: {selectedPlayers.length}</h3>
+            <h3 className="text-center mb-4">Total Cost: {totalCost}</h3>
+            <h3 className="text-center mb-4">Remaining Balance: {remaining}</h3>
             {
                 selectedPlayers.map((player)=>(
-                    <li>{player.name}</li>
+                    <li key={player.id}>{player.name}</li>
                 ))
             }
         </div>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,6 +7,8 @@ const Home = () => {
 
     const [players, setPlayers] = useState([]);
     const [selectedPlayers, setSelectedPlayers] = useState([]);
+    const [remaining, setRemaining] = useState(0);
+    const [totalCost, setTotalCost] = useState(0);
 
     useEffect(() => {
         fetch('./players.json')
@@ -16,16 +18,40 @@ const Home = () => {
     // console.log(players);
 
     const handleSelectBtn = (player) => {
-        setSelectedPlayers([...selectedPlayers, player]);
+        const isExist = selectedPlayers.find((item) => item.id === player.id);
+
+        let count = player.salary;
+
+        if (isExist) {
+            return alert('Already Booked. Try booking another player.')
+        }
+        else {
+            selectedPlayers.forEach((item) => {
+                count += item.salary;
+            });
+            const totalRemaining = 20000 - count;
+
+            if (count > 20000) {
+                return alert("taka sesh!")
+            }
+            else {
+                setTotalCost(count);
+
+                setRemaining(totalRemaining);
+
+                setSelectedPlayers([...selectedPlayers, player]);
+            }
+        }
+
     }
     console.log(selectedPlayers);
 
     return (
         <div className="flex gap-10">
             <div className="grid grid-cols-2 gap-4 w-3/4">
-            {
-                players.map(player => (
-                    
+                {
+                    players.map(player => (
+
                         <div key={player.id} className="border-2 bg-green-200 rounded-2xl p-4 w-96">
                             <div className="flex justify-center">
                                 <img className="rounded-full w-40 h-40" src={player.img} alt="" />
@@ -40,13 +66,15 @@ const Home = () => {
                                 <button onClick={() => handleSelectBtn(player)} className="bg-orange-400 p-3 rounded-xl text-center">Select</button>
                             </div>
                         </div>
-                ))
-            }
+                    ))
+                }
             </div>
 
             <div className="px-5 rounded-xl bg-red-200 w-1/4">
                 <Cart
-                selectedPlayers = {selectedPlayers}
+                    selectedPlayers={selectedPlayers}
+                    remaining={remaining}
+                    totalCost={totalCost}
                 ></Cart>
             </div>
 
